e83e1cfb5c906239324b5466d2742d1c52a10bee
Isa Giongo <isagiongo@yahoo.com.br>
2018-12-02 17:10:45 -0200

    Adiciona configs para gravar testes

--- cypress/integration/sample-spec.js
+++ cypress/integration/sample-spec.js
@@ -1,8 +1,25 @@
-describe('Titulo', () => {
-    it('deve clicar no link Type e validar url', () => {
+describe('Links página inicial', () => {
+    beforeEach(() => {
         cy.visit('https://example.cypress.io')
+    })
+
+    it('deve validar o titulo da pagina', () => {
+        cy.title().should('include', 'Cypress.io: Kitchen Sink')
+    })
+
+    it('deve clicar no link Type e validar url', () => {
         cy.contains('type').click()
         cy.url().should('include', '/commands/actions')
-        cy.get('.action-email').type('teste@gmail.com').should('have.value', 'teste@gmail.com')
     })
+
+    it('deve clicar no link Window e validar url', () => {
+        cy.contains('window').click()
+        cy.url().should('include', 'commands/window')
+    })
+
 })
+
+
+// it('deve validar entrada de email', () => {
+//     cy.get('.action-email').type('teste@gmail.com').should('have.value', 'teste@gmail.com')
+// })
